af74c68119eb8d16353d414a296586dc5816ca3a
TARUN KUMAR <122428140+tarunkumar9923@users.noreply.github.com>
2024-03-01 17:03:08 +0000

    container config done

---
+++ Variable.js
@@ -0,0 +1,9 @@
+ const accountId=9923
+ let accountEmail="Saintarun123@gmail.com"
+ var accountPassword="001"
+ accountCity="Modinagar"
+//  accountId=123 not allowed to change const
+accountEmail="saintarun723@gmail.com"
+accountPassword="0001"
+accountCity="Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity])
